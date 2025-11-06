8d70afc4c9a8ab02c97a3fe68ebac0da8857ae17
Erserady <ernesto.piura@est.ulsa.edu.ni>
2025-11-05 21:37:53 -0600

    actualización de vistas de mesero y cocina v0.2

--- Restaurant-DonPepe-main/RestaurantSoft-main/Frontend/src/views/cook_dashboard/CookDashboard.jsx
+++ Restaurant-DonPepe-main/RestaurantSoft-main/Frontend/src/views/cook_dashboard/CookDashboard.jsx
@@ -1,15 +1,10 @@
 import Header from "../../common/Header";
 import MainComponent from "../../common/MainComponent";
 import { ChefHat } from "lucide-react";
-import { cookNavigationBar } from "../../contracts/NavigationBar";
-import NavigationBar from "../../common/NavigationBar";
-import { useState } from "react";
-import RecipeSection from "../../modules/cook_module/components/RecipeSection";
 import OrderSection from "../../modules/cook_module/components/OrderSection";
 
 const CookDashboard = () => {
   const welcomeTitle = "Panel de Cocinero";
-  const [currentSection, setcurrentSection] = useState("orders");
   const currentView = "cook-dashboard";
 
   return (
@@ -20,18 +15,8 @@ const CookDashboard = () => {
         userRole={<ChefHat />}
       ></Header>
       <main>
-        <NavigationBar
-          SectionState={currentSection}
-          setSectionState={setcurrentSection}
-          currentView={cookNavigationBar}
-          classView={currentView}
-        />
         <section className="main-section">
-          {currentSection === "orders" ? (
-            <OrderSection></OrderSection>
-          ) : (
-            <RecipeSection></RecipeSection>
-          )}
+          <OrderSection></OrderSection>
         </section>
       </main>
     </MainComponent>
--- Restaurant-DonPepe-main/RestaurantSoft-main/Frontend/src/views/waiter_dashboard/OrdersHandlerView.jsx
+++ Restaurant-DonPepe-main/RestaurantSoft-main/Frontend/src/views/waiter_dashboard/OrdersHandlerView.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useNavigate, Outlet, useLocation } from "react-router-dom";
+import { useParams, useNavigate, useLocation } from "react-router-dom";
+import HandleOrder from "../../modules/waiter_module/components/HandleOrder";
 import "../../styles/orders_handler_view.css";
 import {
   Trash2,
@@ -14,100 +15,19 @@ const OrdersHandlerView = () => {
   const location = useLocation();
   const [showNav, setShowNav] = useState(false);
 
-  //Estado de las cuentas
-  const [accounts, setAccounts] = useState([]);
-
-  //Función para agregar cuenta
-  const addAccount = () => {
-    const newAccount = {
-      accountId: crypto.randomUUID(),
-      label: `Cuenta #${accounts.length + 1}`,
-      items: [],
-      subtotal: 0,
-    };
-    setAccounts([...accounts, newAccount]);
-  };
-
-  //Eliminar cuenta
-  const deleteAccount = (id) => {
-    setAccounts((prev) => prev.filter((a) => a.accountId !== id));
-  };
-
-  // Cambiar a una cuenta
-  const HandleCustomOrder = (accountId) => {
-    navigate(`/waiter-dashboard/${tableNumber}/orders-handler/${accountId}`);
-  };
-
-  // Calcular total general
-  const totalPedido = accounts.reduce((acc, a) => acc + a.subtotal, 0);
-
-  // Mantener navegación interna
   useEffect(() => {
     !showNav ? setShowNav(location.pathname.includes("orders-handler/")) : "";
   }, [location, showNav]);
 
+  const HandleCustomOrder = (orderNumber) => {
+    console.log(orderNumber);
+    navigate(`/waiter-dashboard/${tableNumber}/orders-handler/${orderNumber}`);
+  };
+
   return (
     <section className="orders-handler-view">
       <h1 id="content-view">Pedidos para la mesa {tableNumber}</h1>
-      <p className="explain-text-desktop">
-        Selecciona una cuenta en la parte izquierda para agregarle platillos.
-      </p>
-      <p>Si no existe cuenta, click en agregar cuenta.</p>
-
-      <div className="content shadow">
-        {/* === Panel Izquierdo === */}
-        <aside className="orders-container">
-          <div className="orders-header">
-            <h2>Cuentas</h2>
-            <button className="action-btn" onClick={addAccount}>
-              <span>+</span> Agregar Cuenta
-            </button>
-          </div>
-
-          <div className="card-container">
-            {accounts.length === 0 ? (
-              <p>No hay cuentas creadas.</p>
-            ) : (
-              accounts.map((acc, index) => (
-                <article
-                  title="Click para ver detalles"
-                  key={acc.accountId}
-                  onClick={() => HandleCustomOrder(acc.accountId)}
-                  className="order-card shadow"
-                >
-                  <div>
-                    <h3>{acc.label}</h3>
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        deleteAccount(acc.accountId);
-                      }}
-                    >
-                      <Trash2 />
-                    </button>
-                  </div>
-                  <p>Elementos: {acc.items.length}</p>
-                  <p>Subtotal: C${acc.subtotal}</p>
-                </article>
-              ))
-            )}
-          </div>
-
-          <div className="order-footer">
-            <h2>Total del Pedido: C${totalPedido}</h2>
-            <button className="action-btn">Confirmar Pedido</button>
-          </div>
-        </aside>
-
-        <hr className="separator-mobile" />
-
-        {/* === Sección principal === */}
-        <section className="handle-order-container">
-          {/* Pasaremos las funciones y cuentas como contexto */}
-          <Outlet context={{ accounts, setAccounts }} />
-        </section>
-      </div>
-
+      <HandleOrder></HandleOrder>
       <button
         className="return-btn"
         onClick={() => navigate("/waiter-dashboard")}
@@ -115,17 +35,6 @@ const OrdersHandlerView = () => {
         <ArrowLeftToLine size={19} />
         Volver
       </button>
-
-      {showNav && (
-        <nav className="local-nav shadow">
-          <a href="#content-view">
-            <ArrowBigUp />
-          </a>
-          <a href="#order-list">
-            <ArrowBigDown />
-          </a>
-        </nav>
-      )}
     </section>
   );
 };
